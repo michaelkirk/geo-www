9639836a7c622af45639a5e50669d91e80498759
Michael Kirk <michael.code@endoftheworl.de>
2021-12-09 11:52:08 -0800

    wip: can run npm start

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,13 +2,20 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    mode: 'development',
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js' ]
+    },
+    experiments: {
+        // deprecated: TODO switch to `asyncWebAssembly: true,`
+        asyncWebAssembly: true,
+    }
 };
